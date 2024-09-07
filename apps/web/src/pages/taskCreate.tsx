cb7f989f66ffcc6471dfb8cf52cccd1f558558f9
mateuszkrol <mateuszkrol.p@gmail.com>
2024-09-07 23:53:55 +0200

    add task components

--- apps/web/src/pages/taskCreate.tsx
+++ apps/web/src/pages/taskCreate.tsx
@@ -28,27 +28,10 @@ const TaskCreate = () => {
                 <div>
                     {file ? (
                         <div className="relative w-full max-w-md mx-auto overflow-hidden bg-white rounded-xl">
-                            <div className="absolute top-0 right-0 pt-3 pr-3">
-                                <button
-                                    onClick={removeFile}
-                                    type="button"
-                                    className="p-1 text-gray-400 transition-all duration-200 bg-white rounded-md hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
-                                >
-                                    <span className="sr-only">Usuń plik</span>
-                                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                                    </svg>
-                                </button>
-                            </div>
                             <div className="p-8">
                                 <div className="text-center">
-                                    <svg className="w-10 h-10 mx-auto text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                        <path
-                                            className="opacity-75"
-                                            fill="currentColor"
-                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                        ></path>
+                                    <svg className="w-10 h-10 mx-auto text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M5 13l4 4L19 7" />
                                     </svg>
                                     <p className="mt-8 text-xl font-bold text-gray-900">Plik przesłany</p>
                                     <p className="mt-3 text-base font-medium text-gray-600">Nazwa pliku: {file.name}</p>
@@ -100,17 +83,6 @@ const TaskCreate = () => {
                 </div>
             ) : (
                 <div className="relative w-full max-w-md mx-auto overflow-hidden bg-white rounded-xl">
-                    <div className="absolute top-0 right-0 pt-3 pr-3">
-                        <button
-                            type="button"
-                            className="p-1 text-gray-400 transition-all duration-200 bg-white rounded-md hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
-                        >
-                            <span className="sr-only">Close</span>
-                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg>
-                        </button>
-                    </div>
                     <div className="p-8">
                         <div className="text-center">
                             <svg className="w-10 h-10 mx-auto text-gray-900 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -126,14 +98,6 @@ const TaskCreate = () => {
                             <p className="mt-3 text-base font-medium text-gray-600">
                                 Po zakończeniu wgrywania plików, moźesz opuścić okno, a wynik pojawi się w zadaniu.
                             </p>
-                            <div className="mt-8">
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center justify-center w-full px-6 py-4 text-xs font-bold tracking-widest text-gray-900 uppercase transition-all duration-200 bg-transparent border border-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white"
-                                >
-                                    Przejdź do zadania
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
